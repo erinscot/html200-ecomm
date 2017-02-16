62f6912f0afbe9bfc91756d3cb3bb8577d0ea384
Erin Scot <erinmscot@gmail.com>
2017-02-15 16:53:25 -0800

    added back in script.js; trying to get cart pop-up working

--- js/script.js
+++ js/script.js
@@ -61,3 +61,24 @@ function capture() {
   event.preventDefault();
   console.log(document.getElementById("selection").value);
 }
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function showCart() {
+    document.getElementById("cart-content").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.cart-items')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
